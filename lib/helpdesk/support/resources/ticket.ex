0a4e852ee25c2a4168e19a1138455bcbb4b37859
twozeronine <amdusias209@gmail.com>
2023-03-03 22:16:45 +0900

    feat : customizing our Actions

--- lib/helpdesk/support/resources/ticket.ex
+++ lib/helpdesk/support/resources/ticket.ex
@@ -1,13 +1,33 @@
 defmodule Helpdesk.Support.Ticket do
+ @moduledoc """
+  Helpdesk.Support.Ticket
+  |> Ash.Changeset.for_create(:open, %{subject: "My mouse won't click!"})
+  |> Helpdesk.Support.create!()
+ """
+
   use Ash.Resource
 
   actions do
     defaults [:create, :read, :update, :destroy]
+
+    create :open do
+      accept [:subject]
+    end
   end
 
   attributes do
     uuid_primary_key :id
 
-    attribute :subject, :string
+    attribute :subject, :string do
+      allow_nil? false
+    end
+
+    attribute :status, :atom do
+      constraints [one_of: [:open, :closed]]
+
+      default :open
+
+      allow_nil? false
+    end
   end
 end
