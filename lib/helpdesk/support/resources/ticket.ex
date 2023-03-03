3fc9c2d547b600d88d02741f0938b98377e24f05
twozeronine <amdusias209@gmail.com>
2023-03-03 23:19:44 +0900

    feat : Adding basic persistence

--- lib/helpdesk/support/resources/ticket.ex
+++ lib/helpdesk/support/resources/ticket.ex
@@ -5,7 +5,39 @@ defmodule Helpdesk.Support.Ticket do
   |> Helpdesk.Support.create!()
  """
 
-  use Ash.Resource
+
+ @doc """
+    ETS로 데이터레이어가 설정되어있고 간단하게 테스트가 가능하다 !
+
+
+    require Ash.Query
+    for i <- 0..5 do
+      ticket =
+        Helpdesk.Support.Ticket
+        |> Ash.Changeset.for_create(:open, %{subject: "Issue {i}"})
+        |> Helpdesk.Support.create!()
+
+      if rem(i, 2) == 0 do
+        ticket
+        |> Ash.Changeset.for_update(:close)
+        |> Helpdesk.Support.update!()
+      end
+    end
+
+    # Show the tickets where the subject contains "2"
+    Helpdesk.Support.Ticket
+    |> Ash.Query.filter(contains(subject, "2"))
+    |> Helpdesk.Support.read!()
+
+    # Show the tickets that are closed and their subject does not contain "4"
+    Helpdesk.Support.Ticket
+    |> Ash.Query.filter(status == :closed and not(contains(subject, "4")))
+    |> Helpdesk.Support.read!()
+"""
+  use Ash.Resource,
+    data_layer: Ash.DataLayer.Ets
+
+
 
   actions do
     defaults [:create, :read, :update, :destroy]
